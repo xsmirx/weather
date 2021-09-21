01791c8ccb612a40299fab8d110efbfca770e4c6
xsmirx <xsmirx@gmail.com>
2021-09-21 18:46:25 +0300

    rootReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,4 +1,8 @@
 import { combineReducers } from "redux";
 import { appReducer } from "./appReducer/appReducer";
+import { geoReducer } from "./geoReducer/geoReducer";
 
-export const rootReducer = combineReducers({ app: appReducer });
+export const rootReducer = combineReducers({
+  app: appReducer,
+  geo: geoReducer,
+});

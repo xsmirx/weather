7e0577550c0e7e0ee40341f0d11aff16c03f8ce8
xsmirx <xsmirx@gmail.com>
2021-09-24 00:11:44 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Container } from "@mui/material";
+import AlertErrorContainer from "./components/alertError/AlertErrorContainer";
+import LoaderContainer from "./components/Loader/LoaderContainer";
+import WeatherContainer from "./components/Weather/WeatherContainer";
+
 const App = () => {
-  return <h1>Hello, Weather App!</h1>;
+  return (
+    <>
+      <LoaderContainer />
+      <AlertErrorContainer />
+      <Container maxWidth="xs" sx={{ paddingTop: "16px" }}>
+        <WeatherContainer />
+      </Container>
+    </>
+  );
 };
 
 export default App;

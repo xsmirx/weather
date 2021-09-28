54cf5020f117292c6c150c269ac36428ac0d7b01
xsmirx <xsmirx@gmail.com>
2021-09-29 00:25:05 +0300

    new actions

--- src/redux/weatherReducer/actions.js
+++ src/redux/weatherReducer/actions.js
@@ -1,5 +1,7 @@
 //action types
 export const SET_WEATHER = "weatherReducer/SET_WEATHER";
+export const SET_PLACES = "weatherReducer/SET_PLACES";
 
 //action creators
 export const setWeatherAC = (payload) => ({ type: SET_WEATHER, payload });
+export const setPlacesAC = (payload) => ({ type: SET_PLACES, payload });
--- src/redux/weatherReducer/weatherReducer.js
+++ src/redux/weatherReducer/weatherReducer.js
@@ -1,13 +1,28 @@
-import { SET_WEATHER } from "./actions";
+import { SET_PLACES, SET_WEATHER } from "./actions";
 
-const initialState = null;
+const initialState = {
+  weatherCard: {
+    isLoading: false,
+    weatherObject: null,
+  },
+  listPlaces: [],
+};
 
 export const weatherReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_WEATHER:
       return {
         ...state,
-        ...action.payload,
+        listPlaces: [],
+        weatherCard: {
+          ...state.weatherCard,
+          weatherObject: { ...action.payload },
+        },
+      };
+    case SET_PLACES:
+      return {
+        ...state,
+        listPlaces: action.payload.list,
       };
     default:
       return state;
